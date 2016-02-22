3d5923792d8d99f4803235db28a2871e8c0d3608
Bob Familiar <bobf@bluemetal.com>
2016-02-21 21:56:06 -0500

    Preparing for lab
    Removed the parts that will be built through the lab

--- dashboard/Dashboard/MainWindow.xaml.cs
+++ dashboard/Dashboard/MainWindow.xaml.cs
@@ -6,13 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
-using Looksfamiliar.d2c2d.MessageModels;
-using Microsoft.Azure.Devices;
-using Microsoft.ServiceBus.Messaging;
-using Newtonsoft.Json;
 using Microsoft.Maps.MapControl.WPF;
-using Location = Microsoft.Maps.MapControl.WPF.Location;
-using TransportType = Microsoft.Azure.Devices.TransportType;
 
 namespace Looksfamiliar.D2C2D.Dashboard
 {
@@ -21,116 +15,24 @@ namespace Looksfamiliar.D2C2D.Dashboard
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly QueueClient _messageClient;
-        private ServiceClient _serviceClient;
 
         public MainWindow()
         {
             InitializeComponent();
-            _messageClient = QueueClient.CreateFromConnectionString(ConfigurationManager.AppSettings["ServiceBusConnStr"], "messagedrop");
-            _serviceClient = ServiceClient.CreateFromConnectionString(ConfigurationManager.AppSettings["IoTHubConnStr"], TransportType.Amqp);
         }
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            var mapCenter = MyMap.Center;
-
-            var messageTask = Task.Factory.StartNew(() =>
-            {
-                while (true)
-                {
-                    var message = _messageClient.Receive();
-                    var messageBody = string.Empty;
-                    if (message == null) continue;
-
-                    try
-                    {
-                        messageBody = message.GetBody<string>();
-                        var obj = JsonConvert.DeserializeObject<MessageBase>(messageBody);
-                        switch (obj.MessageType)
-                        {
-                            case MessageTypeEnum.NotSet:
-                                throw new Exception("Message Type Not Set");
-                                break;
-                            case MessageTypeEnum.Ping:
-                                var ping = JsonConvert.DeserializeObject<Ping>(messageBody);
-
-                                Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate
-                                {
-                                    var location = new Location(ping.Latitude, ping.Longitude);
-                                    var pin = new Pushpin { Location = location };
-                                    MyMap.Children.Add(pin);
-                                    MyMap.Center = location;
-                                    MyMap.ZoomLevel = 12;
-                                    MyMap.SetView(location, 12);
-                                    MyMap.Focusable = true;
-                                    MyMap.Focus();
-                                    PingFeed.Text += $"Ping Ack: {ping.Ack}\r\n" ;
-                                }));
-
-                                break;
-                            case MessageTypeEnum.Climate:
-                                var climate = JsonConvert.DeserializeObject<Climate>(messageBody);
-                                Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate
-                                {
-                                    TelemetryFeed.Text += $"Timestamp {climate.Timestamp.ToLongDateString()} {climate.Timestamp.ToLongTimeString()}\r\n";
-                                    TelemetryFeed.Text += $"Temperature {climate.Temperature}\r\n";
-                                    TelemetryFeed.Text += $"Humidity {climate.Humidity}\r\n";
-                                    TelemetryFeed.Text += $"Heat Index {climate.HeatIndex}\r\n\r\n";
-                                }));
-                                break;
-                            case MessageTypeEnum.Command:
-                                // noop
-                                break;
-                            default:
-                                throw new ArgumentOutOfRangeException();
-                        }
-
-                        message.Complete();
-                    }
-                    catch (Exception err)
-                    {
-                        Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate
-                        {
-                            // Indicates a problem, unlock message in queue.
-                            TelemetryFeed.Text += err.Message;
-                            TelemetryFeed.Text += messageBody;
-                            message.Abandon();
-                        }));
-                    }
-                }
-            });
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            var climateSettings = new ClimateSettings
-            {
-                MinHumidity = 0,
-                MaxHumiditiy = 100,
-                MinTemperature = 75,
-                MaxTemperature = 110
-            };
 
-            var command = new Command
-            {
-                CommandType = CommandTypeEnum.Start,
-                CommandParameters = JsonConvert.SerializeObject(climateSettings)
-            };
-
-            var json = JsonConvert.SerializeObject(command);
-            var message = new Message(Encoding.ASCII.GetBytes(json));
-
-            _serviceClient.SendAsync(ConfigurationManager.AppSettings["DeviceId"], message);
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            var command = new Command();
-            command.CommandType = CommandTypeEnum.Stop;
-            var json = JsonConvert.SerializeObject(command);
-            var message = new Message(Encoding.ASCII.GetBytes(json));
-            _serviceClient.SendAsync(ConfigurationManager.AppSettings["DeviceId"], message);
+
         }
     }
 }
--- device/Device/MainPage.xaml.cs
+++ device/Device/MainPage.xaml.cs
@@ -4,8 +4,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Looksfamiliar.d2c2d.MessageModels;
-using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
 
 namespace Looksfamiliar.Device.Win10CoreIoT
@@ -16,176 +14,15 @@ namespace Looksfamiliar.Device.Win10CoreIoT
         private const string DeviceId = "[device id]";
         private const string DeviceKey = "[device key]";
 
-        private static Location _location;
-        private static DeviceClient _deviceClient;
-
-        private static Task _pingTask;
-        private static Task _listenTask;
-        private static Task _telemetryTask;
-        private static bool _sendingTelemetry = false;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private async void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
         {
             Status.Text = "Main Page Loaded";
-
-            _location = await GetLocationAsync();
-
-            Status.Text = $"Location is {_location.city}, {_location.country}";
-
-            try
-            {
-                _deviceClient = DeviceClient.Create(IotHubUri,
-                    AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey),
-                    TransportType.Http1);
-
-                Status.Text = "IoT Hub Connected";
-            }
-            catch (Exception connectionErr)
-            {
-                Status.Text = connectionErr.Message;
-            }
-
-            StartPingTask(Status);
-
-            StartListenTask(Status);
         }
 
-        private static async Task<Location> GetLocationAsync()
-        {
-            var client = new HttpClient();
-            var json = await client.GetStringAsync("http://ip-api.com/json");
-            var location = JsonConvert.DeserializeObject<Location>(json);
-            return location;
-        }
-
-        private static void StartPingTask(TextBox status)
-        {
-            _pingTask = Task.Factory.StartNew(async () =>
-            {
-                while (true)
-                {
-                    var ping = new Ping
-                    {
-                        Ack = "Windows 10 Core IoT Device is Alive",
-                        Longitude = _location.lon,
-                        Latitude = _location.lat
-                    };
-
-                    var json = JsonConvert.SerializeObject(ping);
-
-                    var message = new Message(Encoding.ASCII.GetBytes(json));
-
-                    try
-                    {
-                        await _deviceClient.SendEventAsync(message);
-                    }
-                    catch (Exception err)
-                    {
-                        var errMessage = err.Message;
-                        status.Text = errMessage;
-                    }
-
-                    await Task.Delay(60000);
-                }
-            });
-        }
-
-        private static void StartListenTask(TextBox status)
-        {
-            _listenTask = Task.Factory.StartNew(async () =>
-            {
-                while (true)
-                {
-                    var message = await _deviceClient.ReceiveAsync();
-
-                    if (message == null)
-                        continue;
-
-                    var json = Encoding.ASCII.GetString(message.GetBytes());
-
-                    var command = JsonConvert.DeserializeObject<Command>(json);
-
-                    switch (command.CommandType)
-                    {
-                        case CommandTypeEnum.Ping:
-
-                            var ping = new Ping
-                            {
-                                Ack = "Windows 10 Core IoT Device is Alive",
-                                Longitude = _location.lon,
-                                Latitude = _location.lat
-                            };
-
-                            json = JsonConvert.SerializeObject(ping);
-
-                            var pingMessage = new Message(Encoding.ASCII.GetBytes(json));
-
-                            try
-                            {
-                                await _deviceClient.SendEventAsync(pingMessage);
-                            }
-                            catch (Exception err)
-                            {
-                                var errMessage = err.Message;
-                                // not sure this will work as we are executing on bacground thread and UI element is on UI thread
-                                status.Text = errMessage; 
-                            }
-                            break;
-                        case CommandTypeEnum.Start:
-                            var settings = JsonConvert.DeserializeObject<ClimateSettings>(command.CommandParameters);
-                            _sendingTelemetry = true;
-                            StartTelemetry(settings, status);
-                            break;
-                        case CommandTypeEnum.Stop:
-                            _sendingTelemetry = false;
-                            break;
-                        case CommandTypeEnum.UpdateFirmeware:
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-
-                    await _deviceClient.CompleteAsync(message);
-                }
-            });
-        }
-
-        private static void StartTelemetry(ClimateSettings settings, TextBox status)
-        {
-            _telemetryTask = Task.Factory.StartNew(async () =>
-            {
-                while (_sendingTelemetry)
-                {
-                    var random = new Random();
-
-                    var climate = new Climate
-                    {
-                        Temperature = random.Next((int) settings.MinTemperature, (int) settings.MaxTemperature),
-                        Humidity = random.Next((int) settings.MinHumidity, (int) settings.MaxHumiditiy)
-                    };
-
-                    var json = JsonConvert.SerializeObject(climate);
-
-                    var message = new Message(Encoding.ASCII.GetBytes(json));
-
-                    try
-                    {
-                        await _deviceClient.SendEventAsync(message);
-                    }
-                    catch (Exception err)
-                    {
-                        var errMessage = err.Message;
-                        status.Text = errMessage;
-                    }
-
-                    await Task.Delay(5000);
-                }
-            });
-        }
     }
 }
